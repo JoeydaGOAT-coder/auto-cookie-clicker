7d60ddd5efadc4321620abb6d6c5403358f3b39a
JoeydaGOAT-coder <joey@kilians.us>
2025-08-27 16:31:45 -0500

    Update allScripts.js
    added script for automatically upgrading buildings with sugar lumps

--- plain-scripts/allScripts.js
+++ plain-scripts/allScripts.js
@@ -1,6 +1,7 @@
 setInterval(function () {
-  Game.ClickCookie();
+  Game.ClickCookie();//automatically clicks the big cookie
 
+	//automatically buys upgrades
   for (var i in Game.Upgrades) {
     var upgrade = Game.Upgrades[i];
       if (upgrade.unlocked && !upgrade.bought && Game.cookies >= upgrade.getPrice()) {
@@ -8,14 +9,27 @@ setInterval(function () {
       }
   }
 
+	//automatically buys buildings
   for (var i in Game.Objects) {
     var building = Game.Objects[i];
     if (Game.cookies >= building.getPrice()) {
       building.buy();
     }
   }
-  
+
+	//automatically clicks golden cookies and other shines
 	Game.shimmers.forEach(function(shimmer) { shimmer.pop(); });
 
+	//automatically collects sugar lumps when ripe
 	Game.clickLump();
+
+	//automatically upgrades buildings with sugar lumps
+	for (var i = 0; i < Game.ObjectsById.length; i++) {
+    var building = Game.ObjectsById[i];
+    if (building.level < 10 && building.level <= Game.lumps) {
+      if (typeof building.levelUp !== 'undefined') {
+        building.levelUp();
+      }
+    }
+  }
 }, 25);
