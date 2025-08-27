821066131624f5e13a0074e1983ddf52eaf2cdc5
JoeydaGOAT-coder <joey@kilians.us>
2025-08-27 06:40:26 -0500

    Create allScripts.js
    combined other scripts into one

---
+++ plain-scripts/allScripts.js
@@ -0,0 +1,19 @@
+setInterval(function () {
+  Game.ClickCookie();
+
+  for (var i in Game.Upgrades) {
+    var upgrade = Game.Upgrades[i];
+      if (upgrade.unlocked && !upgrade.bought && Game.cookies >= upgrade.getPrice()) {
+        upgrade.buy();
+      }
+  }
+
+  for (var i in Game.Objects) {
+    var building = Game.Objects[i];
+    if (Game.cookies >= building.getPrice()) {
+      building.buy();
+    }
+  }
+  
+	Game.shimmers.forEach(function(shimmer) { shimmer.pop(); });
+}, 25);
