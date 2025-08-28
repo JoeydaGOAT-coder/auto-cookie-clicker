912ce9754cc1aeada1f6deb22a6533f9b388b90d
JoeydaGOAT-coder <joey@kilians.us>
2025-08-28 06:20:25 -0500

    Update autoAscend.js
    fixed an error

--- plain-scripts/autoAscend.js
+++ plain-scripts/autoAscend.js
@@ -1,7 +1,7 @@
 let prestigeLevel = 1;
 setInterval(function () {
   if (Game.ascensionMode === 0) {
-    if (((Game.cookiesEarned/1000000000000)**(1/3))**(1/2) === prestigeLevel) {
+    if (((Game.cookiesEarned/1000000000000)**(1/3))**(1/2) >= prestigeLevel) {
       Game.Ascend(true);
       prestigeLevel ++
     }
