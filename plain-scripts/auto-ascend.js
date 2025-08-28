1fb212b9c3c7b9a2b2637ccd3424f0f82c3aac00
JoeydaGOAT-coder <joey@kilians.us>
2025-08-27 19:33:38 -0500

    Create auto-ascend.js
    added script to automatically ascend

---
+++ plain-scripts/auto-ascend.js
@@ -0,0 +1,9 @@
+let prestigeLevel = 1;
+setInterval(function () {
+  if (Game.ascensionMode === 0) {
+    if (((Game.cookiesEarned/1000000000000)**(1/3))**(1/2) === prestigeLevel) {
+      Game.Ascend();
+      prestigeLevel ++
+    }
+  }
+}, 25);
