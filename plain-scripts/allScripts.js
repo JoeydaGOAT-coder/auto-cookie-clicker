1c68ff190eca37422f50e364971cc6330ae3e112
JoeydaGOAT-coder <joey@kilians.us>
2025-08-27 19:39:18 -0500

    Update allScripts.js
    added script to automatically prestige

--- plain-scripts/allScripts.js
+++ plain-scripts/allScripts.js
@@ -1,35 +1,44 @@
+let prestigeLevel = 1
 setInterval(function () {
-  Game.ClickCookie();//automatically clicks the big cookie
+	if (Game.ascensionValue === 0) {
+	  Game.ClickCookie();//automatically clicks the big cookie
+	
+		//automatically buys upgrades
+	  for (var i in Game.Upgrades) {
+	    var upgrade = Game.Upgrades[i];
+	      if (upgrade.unlocked && !upgrade.bought && Game.cookies >= upgrade.getPrice()) {
+	        upgrade.buy();
+	      }
+	  }
+	
+		//automatically buys buildings
+	  for (var i in Game.Objects) {
+	    var building = Game.Objects[i];
+	    if (Game.cookies >= building.getPrice()) {
+	      building.buy();
+	    }
+	  }
+	
+		//automatically clicks golden cookies and other shines
+		Game.shimmers.forEach(function(shimmer) { shimmer.pop(); });
+	
+		//automatically collects sugar lumps when ripe
+		Game.clickLump();
+	
+		//automatically upgrades buildings with sugar lumps
+		for (var i = 0; i < Game.ObjectsById.length; i++) {
+	    var building = Game.ObjectsById[i];
+	    if (building.level < 10 && building.level <= Game.lumps) {
+	      if (typeof building.levelUp !== 'undefined') {
+	        building.levelUp();
+	      }
+	    }
+	  }
 
-	//automatically buys upgrades
-  for (var i in Game.Upgrades) {
-    var upgrade = Game.Upgrades[i];
-      if (upgrade.unlocked && !upgrade.bought && Game.cookies >= upgrade.getPrice()) {
-        upgrade.buy();
-      }
-  }
-
-	//automatically buys buildings
-  for (var i in Game.Objects) {
-    var building = Game.Objects[i];
-    if (Game.cookies >= building.getPrice()) {
-      building.buy();
+		//automatically ascends when you get at least one prestige level
+		if (((Game.cookiesEarned/1000000000000)**(1/3))**(1/2) === prestigeLevel) {
+      Game.Ascend(true);
+      prestigeLevel ++;
     }
-  }
-
-	//automatically clicks golden cookies and other shines
-	Game.shimmers.forEach(function(shimmer) { shimmer.pop(); });
-
-	//automatically collects sugar lumps when ripe
-	Game.clickLump();
-
-	//automatically upgrades buildings with sugar lumps
-	for (var i = 0; i < Game.ObjectsById.length; i++) {
-    var building = Game.ObjectsById[i];
-    if (building.level < 10 && building.level <= Game.lumps) {
-      if (typeof building.levelUp !== 'undefined') {
-        building.levelUp();
-      }
-    }
-  }
+	}
 }, 25);
